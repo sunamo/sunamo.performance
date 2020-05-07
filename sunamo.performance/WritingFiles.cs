ac9a9156d201a603aea021152d8b1ec413ec3f9b
Radek Jancik <ed@jepsano.net>
2020-05-07 13:12:41 +0200

    Mass commit 7-5-2020

--- sunamo.performance/WritingFiles.cs
+++ sunamo.performance/WritingFiles.cs
@@ -13,7 +13,7 @@ File.WriteAllText takes 8ms
          */
 
         var file = @"d:\_Test\sunamo.performance\WritingSpeed\a.txt";
-        var content = string.Empty.PadLeft(1000000, AllChars.at);
+        var content = string.Empty.PadLeft(1000000, AllChars.commat);
 
         StopwatchStatic.Start();
         TF.SaveFile(content, file);
