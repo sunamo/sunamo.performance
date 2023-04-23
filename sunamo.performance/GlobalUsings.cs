c593a1dcc7cddf89c998d7b8af3a9227c197360f
radek.jancik@sunamo.cz <radek.jancik@sunamo.cz>
2023-04-23 16:27:22 +0200

    Knowledge is power.

---
+++ sunamo.performance/GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using cmd.Essential;
+global using sunamo.Essential;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.Globalization;
+global using System.IO;
+global using System.Linq;
+global using System.Runtime.InteropServices;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Threading;
+global using System;
